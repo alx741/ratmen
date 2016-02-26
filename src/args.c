8922ce7b5163ed1454dfa79bb0f4dc27818cc9da
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-02-25 22:19:00 -0500

    Split arguments parser

--- src/args.c
+++ src/args.c
@@ -26,6 +26,8 @@ char *shell          = "/bin/sh";
 char *titlename      = NULL;
 bool   unfocus_exit  = Undef;
 
+set_args(char option);
+
 
 int parse_args(int argc, char **argv)
 {
@@ -67,147 +69,154 @@ int parse_args(int argc, char **argv)
             break;
         }
 
-        switch (option)
-        {
-            case 'A': /* --align {left|center|right} */
-                if (strcasecmp(optarg, "left") == 0)
-                {
-                    align = left;
-                }
-                else if (strcasecmp(optarg, "center") == 0)
-                {
-                    align = center;
-                }
-                else if (strcasecmp(optarg, "right") == 0)
-                {
-                    align = right;
-                }
-                else
-                {
-                    char buffer[200] = "";
-                    sprintf(buffer, "unknown align argument `%s' "
-                            "(should be `left', `center' or `right')", optarg);
-                    die(buffer);
-                }
-                break;
-
-            case 'b': /* -b, --back PREVMENU */
-                prevmenu = optarg;
-                break;
-
-            case 'B': /* --background BGCOLOR */
-                bgcname = optarg;
-                break;
-
-            case 'c': /* -c, --align=center */
-                align = center;
-                break;
-
-            case 'C': /* -C, --class CLASSNAME */
-                classname = optarg;
-                break;
-
-            case 'X': /* --debug */
-                debug = true;
-                break;
-
-            case 'd': /* -d, --delimiter DELIM */
-                delimiter = optarg;
-                if (strcmp(delimiter, "") == 0)
-                {
-                    die("delimiter must be at least one character long");
-                }
-                break;
-
-            case 'D': /* -D, --display DISPLAYNAME */
-                displayname = optarg;
-                break;
-
-            case 'F': /* -F, --font FNAME */
-                fontname = optarg;
-                break;
-
-            case 'E': /* --foreground FGCOLOR */
-                fgcname = optarg;
-                break;
-
-            case 'h': /* -h, --help */
-                print_help();
-                break;
-
-            case 'i': /* -i, --item POSITION */
-                cur_item = atoi(optarg) - 1;
-                break;
-
-            case 'm': /*     --mouse */
-                mouse_on = true;
-                break;
-
-            case 'M': /*     --no-mouse */
-                mouse_on = false;
-                break;
-
-            case 'l': /* -l, --align=left */
-                align = left;
-                break;
-
-            case 'p': /* -p, --print */
-                output = print;
-                break;
-
-            case 'o': /* -o, --scroll-offset ITEMS */
-                scroll_offset = atoi(optarg);
-                break;
-
-            case 'r': /* -r, --align=right */
-                align = right;
-                break;
-
-            case 'S': /* -S, --shell SHELL */
-                shell = optarg;
-                break;
-
-            case 's': /* -s, --style {snazzy|dreary} */
-                if (strcasecmp(optarg, "dreary") == 0)
-                {
-                    style = dreary;
-                }
-                else if (strcasecmp(optarg, "snazzy") == 0)
-                {
-                    style = snazzy;
-                }
-                else
-                {
-                    char buffer[200] = "";
-                    sprintf(buffer, "unknown style argument `%s' "
-                            "(should be `snazzy' or `dreary')", optarg);
-                    die(buffer);
-                }
-                break;
-
-            case 't': /* -t, --title NAME */
-                titlename = optarg;
-                break;
-
-            case 'u': /* -u, --unfocus-exit */
-                unfocus_exit = true;
-                break;
-
-            case 'U': /* -U, --no-unfocus-exit */
-                unfocus_exit = false;
-                break;
-
-            case 'V': /* -V, --version */
-                print_version();
-                break;
-
-            case '?':
-                /* getopt_long already printed an error message. */
-                break;
-
-            default:
-                abort();
-        }
+        set_args(option);
     }
+
     return (optind);
+}
+
+
+set_args(char option)
+{
+    switch (option)
+    {
+        case 'A': /* --align {left|center|right} */
+            if (strcasecmp(optarg, "left") == 0)
+            {
+                align = left;
+            }
+            else if (strcasecmp(optarg, "center") == 0)
+            {
+                align = center;
+            }
+            else if (strcasecmp(optarg, "right") == 0)
+            {
+                align = right;
+            }
+            else
+            {
+                char buffer[200] = "";
+                sprintf(buffer, "unknown align argument `%s' "
+                        "(should be `left', `center' or `right')", optarg);
+                die(buffer);
+            }
+            break;
+
+        case 'b': /* -b, --back PREVMENU */
+            prevmenu = optarg;
+            break;
+
+        case 'B': /* --background BGCOLOR */
+            bgcname = optarg;
+            break;
+
+        case 'c': /* -c, --align=center */
+            align = center;
+            break;
+
+        case 'C': /* -C, --class CLASSNAME */
+            classname = optarg;
+            break;
+
+        case 'X': /* --debug */
+            debug = true;
+            break;
+
+        case 'd': /* -d, --delimiter DELIM */
+            delimiter = optarg;
+            if (strcmp(delimiter, "") == 0)
+            {
+                die("delimiter must be at least one character long");
+            }
+            break;
+
+        case 'D': /* -D, --display DISPLAYNAME */
+            displayname = optarg;
+            break;
+
+        case 'F': /* -F, --font FNAME */
+            fontname = optarg;
+            break;
+
+        case 'E': /* --foreground FGCOLOR */
+            fgcname = optarg;
+            break;
+
+        case 'h': /* -h, --help */
+            print_help();
+            break;
+
+        case 'i': /* -i, --item POSITION */
+            cur_item = atoi(optarg) - 1;
+            break;
+
+        case 'm': /*     --mouse */
+            mouse_on = true;
+            break;
+
+        case 'M': /*     --no-mouse */
+            mouse_on = false;
+            break;
+
+        case 'l': /* -l, --align=left */
+            align = left;
+            break;
+
+        case 'p': /* -p, --print */
+            output = print;
+            break;
+
+        case 'o': /* -o, --scroll-offset ITEMS */
+            scroll_offset = atoi(optarg);
+            break;
+
+        case 'r': /* -r, --align=right */
+            align = right;
+            break;
+
+        case 'S': /* -S, --shell SHELL */
+            shell = optarg;
+            break;
+
+        case 's': /* -s, --style {snazzy|dreary} */
+            if (strcasecmp(optarg, "dreary") == 0)
+            {
+                style = dreary;
+            }
+            else if (strcasecmp(optarg, "snazzy") == 0)
+            {
+                style = snazzy;
+            }
+            else
+            {
+                char buffer[200] = "";
+                sprintf(buffer, "unknown style argument `%s' "
+                        "(should be `snazzy' or `dreary')", optarg);
+                die(buffer);
+            }
+            break;
+
+        case 't': /* -t, --title NAME */
+            titlename = optarg;
+            break;
+
+        case 'u': /* -u, --unfocus-exit */
+            unfocus_exit = true;
+            break;
+
+        case 'U': /* -U, --no-unfocus-exit */
+            unfocus_exit = false;
+            break;
+
+        case 'V': /* -V, --version */
+            print_version();
+            break;
+
+        case '?':
+            /* getopt_long already printed an error message. */
+            break;
+
+        default:
+            abort();
+    }
 }
